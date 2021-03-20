531c6fef1f2d1c68cef15a71eb63e27aa5b772a2
Mekyle Logen Naidoo <mekylenaidoo@yahoo.com>
2021-03-20 07:32:24 +0200

    Setting up the environment

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# SpookyDAO
-A port of MolochDAO v2 on the Casper Network
+# svelte-tailwind
+A project template using Svelte, Tailwind CSS, PostCSS and Webpack
+
+The base webpack configuration is based on [start template project 🎒](https://github.com/davi-mbatista/start)
+
+To run locally
+````bash
+npm run start
+````
+
+To make a production build
+````bash
+npm run build
+````
---
+++ src/index.svelte
@@ -0,0 +1,24 @@
+<div class="p-5 h-screen flex justify-center items-center">
+
+    <section class="text-2xl text-gray-600">
+        <span class="p-2 bg-red-600 text-white rounded-lg shadow-sm">
+            Svelte
+        </span>
+        {' '}+
+        <span class="p-2 bg-green-600 text-white rounded-lg shadow-sm">
+            Tailwind CSS
+        </span>
+    </section>
+
+    <footer class="flex absolute bottom-0 mb-5 text-gray-700">
+        <p>Powered by</p>
+        <a
+            class="ml-1 text-gray-800 underline"
+            href="https://github.com/davi-mbatista/start"
+            alt="GitHub's start template project"
+            target="_blank"
+            rel="noopener noreferrer">
+            /start
+        </a>
+    </footer>
+</div>
