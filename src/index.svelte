8ce7b955264c05c6eb874955eaff44ce56b21c6f
Mekyle Logen Naidoo <mekylenaidoo@yahoo.com>
2021-03-22 18:17:10 +0200

    Update index.svelte

--- src/index.svelte
+++ src/index.svelte
@@ -5,10 +5,10 @@
 <div class="container mx-auto">
     <div class="flex">
         <div class="flex-1 bg-white h-screen">
-            <div class="block bg-white h-2/4">1</div>
-            <div class="block bg-red-400 h-2/4">2</div>
+            <div class="block bg-white h-3/4"></div>
+            <div class="block bg-red-400 h-1/4"></div>
         </div>
-        <div class="flex-1 bg-blue-400 h-screen">3</div>
-        <div class="flex-1 bg-indigo-600 h-screen">4</div>
+        <div class="flex-1 bg-blue-400 h-screen"></div>
+        <div class="flex-1 bg-indigo-600 h-screen"></div>
     </div>
 </div>
